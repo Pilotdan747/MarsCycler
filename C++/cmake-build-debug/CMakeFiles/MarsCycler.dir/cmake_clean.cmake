d6567f8df8fa137fff5557ebc043173fcc3947b4
Daniel Owen <pilotdan747@gmail.com>
2019-05-13 12:55:49 -0400

    clion build files
    FIles from a build on clion - might add gitignore later

---
+++ C++/cmake-build-debug/CMakeFiles/MarsCycler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MarsCycler.dir/main.cpp.o"
+  "MarsCycler.pdb"
+  "MarsCycler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MarsCycler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
